76415d9695e96ad160c7d02aeb7f384f7f34753a
Jossy2005 <jab.mena@yavirac.edu.ec>
2025-04-24 22:00:41 -0500

    Cambios

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'auth/login', pathMatch: 'full' }, // 🔁 redirección automática a /auth
+  { path: '', redirectTo: 'auth/identidad', pathMatch: 'full' },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(adm => adm.AdminModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(aut => aut.AuthModule) }
 ];
--- front/src/app/auth/auth-routing.module.ts
+++ front/src/app/auth/auth-routing.module.ts
@@ -2,30 +2,34 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { IdentidadComponent } from './components/identidad/identidad.component'; // Asegúrate de importar
 
 const routes: Routes = [
   {
-    path:"",
-    redirectTo:'login',
+    path: '',
+    redirectTo: 'identidad',
     pathMatch: 'full'
   },
   {
-    path:"login",
+    path: 'identidad',
+    component: IdentidadComponent
+  },
+  {
+    path: 'login',
     component: LoginComponent
   },
   {
-    path:"register",
-    component:RegisterComponent
+    path: 'register',
+    component: RegisterComponent
   },
-
   {
-    path:"**",
-    redirectTo: 'login'
-  },
+    path: '**',
+    redirectTo: 'identidad'
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
